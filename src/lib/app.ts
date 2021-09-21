a7f68cf3058f72066f2f2652890b28b3fed12877
devon-wolf <devon.wolfkiel@gmail.com>
2021-09-21 10:48:57 -0700

    Set up basic example query, add nodemon

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,1 +1,4 @@
+/*
+    working through tutorial here: https://www.howtographql.com/graphql-js/1-getting-started/
+*/
 
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
+/*
+    working through tutorial here: https://www.howtographql.com/graphql-js/1-getting-started/
+*/
 
+import { ApolloServer } from 'apollo-server';
+
+const typeDefs = `
+    type Query {
+        info: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        info: () => 'This is my API'
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server
+    .listen()
+    .then(({ url }) => 
+        console.log(`Server is running on ${url}`));
