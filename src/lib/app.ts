66e10463bbe08b36b3d5ba50cb4605ad456288c0
devon-wolf <devon.wolfkiel@gmail.com>
2021-09-21 10:54:40 -0700

    Add non-listening logic for apollo server to app

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -2,3 +2,13 @@
     working through tutorial here: https://www.howtographql.com/graphql-js/1-getting-started/
 */
 
+import { ApolloServer } from 'apollo-server';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+export default server;
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,7 @@
     working through tutorial here: https://www.howtographql.com/graphql-js/1-getting-started/
 */
 
-import { ApolloServer } from 'apollo-server';
-import typeDefs from './lib/graphql/typeDefs';
-import resolvers from './lib/graphql/resolvers';
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
+import server from './lib/app';
 
 server
     .listen()
