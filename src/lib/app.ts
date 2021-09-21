28e61ad291dbd0c380aa656a4cc6e3f1a0046e97
devon-wolf <devon.wolfkiel@gmail.com>
2021-09-21 14:03:19 -0700

    Add hacky script-wrangling to copy schema into dist folder

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,13 +1,15 @@
 /*
     working through tutorial here: https://www.howtographql.com/graphql-js/1-getting-started/
 */
-
+import fs from 'fs';
+import path from 'path';
 import { ApolloServer } from 'apollo-server';
-import schema from './graphql/schema';
 import resolvers from './graphql/resolvers';
 
 const server = new ApolloServer({
-    typeDefs: schema,
+    typeDefs: fs.readFileSync(
+        path.join(`${__dirname}/graphql/`, 'schema.graphql'), 'utf8'
+    ),
     resolvers
 });
 
