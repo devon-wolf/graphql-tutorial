9880fae686f1f1edfd12bb9bf98b1aa700a507bb
devon-wolf <devon.wolfkiel@gmail.com>
2021-09-21 13:33:46 -0700

    Add some notes

--- src/lib/graphql/resolvers.ts
+++ src/lib/graphql/resolvers.ts
@@ -6,11 +6,15 @@ const resolvers = {
         info: (): string => 'This is my API',
         feed: (): Link[] => links
     },
+
     Link: {
         id: (parent: Link): string => parent.id,
         description: (parent: Link): string => parent.description,
         url: (parent: Link): string => parent.url
     }
+
+    // parent = result of the previous resolver execution level
+    // this 'Link' resolver is not actually needed in this very simple case; it's mostly illustrating the chain of resolvers
 };
 
 export default resolvers;
